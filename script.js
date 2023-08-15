1186b2f421d5bd8077e633f5938100dc3954251d
Joseph Le <13lejoseph@gmail.com>
2023-08-15 15:44:30 -0700

    add create card function

--- script.js
+++ script.js
@@ -16,7 +16,30 @@ const newBook = (title, author, pages, read) => {
 const book = new Book(title, author, pages, read)
 myLibrary.push(book)
 }
+const createCard = () => {
+    const div = document.createElement('div')
+    div.className = 'example-cards'
 
+    const title = document.createElement('p')
+    const titleText = document.createTextNode('hello world')
+    title.appendChild(titleText)
+
+    const author = document.createElement('p')
+    const authorText = document.createTextNode('jk rowling')
+    author.appendChild(authorText)
+
+    const pages = document.createElement('p')
+    const pagesText = document.createTextNode('392')
+    pages.appendChild(pagesText)
+
+    const read = document.createElement('p')
+    const readText = document.createTextNode('read')
+    read.appendChild(readText)
+
+    document.getElementById('cards-container').appendChild(div)
+    div.append(title, author, pages, read)
+    
+}
 //remove book from library
 const removeBook = () => {
 
