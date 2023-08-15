e44b4b46659b8e4688fc92fa5f6051e39089082b
Joseph Le <13lejoseph@gmail.com>
2023-08-15 16:39:27 -0700

    tweak book cards

--- script.js
+++ script.js
@@ -16,28 +16,33 @@ const newBook = (title, author, pages, read) => {
 const book = new Book(title, author, pages, read)
 myLibrary.push(book)
 }
+
 const createCard = () => {
     const div = document.createElement('div')
     div.className = 'example-cards'
 
     const title = document.createElement('p')
-    const titleText = document.createTextNode('hello world')
+    const titleText = document.createTextNode('"The Chronicles of Narnia: The Lion the Witch and the Wardrobe"')
     title.appendChild(titleText)
 
     const author = document.createElement('p')
-    const authorText = document.createTextNode('jk rowling')
+    const authorText = document.createTextNode('By: ' + 'jk rowling')
     author.appendChild(authorText)
 
     const pages = document.createElement('p')
-    const pagesText = document.createTextNode('392')
+    const pagesText = document.createTextNode('392' + ' pages')
     pages.appendChild(pagesText)
 
-    const read = document.createElement('p')
-    const readText = document.createTextNode('read')
+    const read = document.createElement('button')
+    const readText = document.createTextNode('Read?')
     read.appendChild(readText)
 
+    const removeBook = document.createElement('button')
+    const removeBookText = document.createTextNode('Remove Book')
+    removeBook.appendChild(removeBookText)
+
     document.getElementById('cards-container').appendChild(div)
-    div.append(title, author, pages, read)
+    div.append(title, author, pages, read, removeBook)
     
 }
 //remove book from library
